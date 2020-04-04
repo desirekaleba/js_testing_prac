614042177ad624a46afa3177dcfdc847ad87b202
desirekaleba <desirekaleba@gmail.com>
2020-04-04 20:52:30 +0300

    assert.typeOf

--- test/appTest.js
+++ test/appTest.js
@@ -1,8 +1,14 @@
 const assert = require('chai').assert;
-const app = require('../app');
+const sayHello = require('../app').sayHello;
 
 describe('App', () => {
-    it('app should return hello', () => {
-        assert.equal(app(), 'hello');
+    it('sayHello should return hello', () => {
+        let result = sayHello();
+        assert.equal(result, 'hello');
+    });
+
+    it('sayHello should return type string', function(){
+        let result = sayHello();
+        assert.typeOf(result, 'string');
     });
 });
