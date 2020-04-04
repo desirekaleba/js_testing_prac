c906b229388aefb5424ff0b0bfdf04f9686f4702
desirekaleba <desirekaleba@gmail.com>
2020-04-04 21:02:54 +0300

    isAbove

--- test/appTest.js
+++ test/appTest.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
 const sayHello = require('../app').sayHello;
+const addNumbers = require('../app').addNumbers;
 
 describe('App', () => {
     it('sayHello should return hello', () => {
@@ -10,5 +11,15 @@ describe('App', () => {
     it('sayHello should return type string', function(){
         let result = sayHello();
         assert.typeOf(result, 'string');
+    });
+
+    it('addNumbers should return the sum of two numbers', function() {
+        let result = addNumbers(4, 5);
+        assert.equal(result, 9);
+    });
+
+    it('addNumbers should be above 7', () => {
+        let result = addNumbers(-4, 12);
+        assert.isAbove(result, 7);
     });
 });
